13d0539e57c4be2f04644b72db9232b35b6a3a16
Kristian Klette <geferg.dev@gmail.com>
2023-11-14 04:11:32 +0100

    Make TODO list

--- SentralLibrary/Central.cs
+++ SentralLibrary/Central.cs
@@ -7,5 +7,13 @@ using System.Threading.Tasks;
 namespace SentralLibrary;
 public class Central
 {
+    //TODO hook up database
 
+    //TODO hook up card reader access requests
+
+    //TODO report generator
+
+    //TODO make admin methods/fields
+
+    //TODO make alarm triggers (event)
 }
