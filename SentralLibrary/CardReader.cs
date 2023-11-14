a7968b6bdcbbd2d5e44b58237a0d3104579992c6
Kristian Klette <geferg.dev@gmail.com>
2023-11-14 02:42:01 +0100

    Initial commit

---
+++ SentralLibrary/CardReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentralLibrary;
+internal class CardReader
+{
+    public int ReaderNumber { get; set; }
+    public int Placement {  get; set; }
+}
---
+++ SentralLibrary/UserData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentralLibrary;
+internal class UserData
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public int CardID { get; private set; }
+    public int CardPin { get; private set; }
+    public (DateTime start, DateTime end) ValidityPeriod { get; private set; }
+
+    public UserData(string firstName, string lastName, string email, int cardID, int cardPin, DateTime startTime, DateTime endTime)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        CardID = cardID;
+        CardPin = cardPin;
+        ValidityPeriod = (startTime, endTime);
+    }
+}
