f234209637957cc0ac5fe997c59fefce4abfcff3
Kristian Klette <geferg.dev@gmail.com>
2023-11-15 03:56:25 +0100

    Add console project

---
+++ Kortleser/CardReaderConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CardReaderConsoleUI;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        CardReaderLibrary.CardReader reader = new();
+
+        Console.WriteLine("Hello, World!");
+
+        while (true)
+        {
+            string? userInput = Console.ReadLine();
+            if (string.IsNullOrEmpty(userInput))
+            {
+                break;
+            }
+
+            userInput.Split(' ');
+        }
+    }
+}
