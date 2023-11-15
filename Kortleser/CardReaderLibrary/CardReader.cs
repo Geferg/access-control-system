4272fc6dde46ed67e01612a0d831e4b97441908d
Kristian Klette <geferg.dev@gmail.com>
2023-11-15 01:16:06 +0100

    Create command code methods

--- Kortleser/CardReaderLibrary/CardReader.cs
+++ Kortleser/CardReaderLibrary/CardReader.cs
@@ -7,4 +7,69 @@ using System.Threading.Tasks;
 namespace CardReaderLibrary;
 internal class CardReader
 {
+
+
+    public void ForceMessage()
+    {
+        SendCommand("$R1");
+    }
+
+    public bool ChangeNode(int nodeNumber)
+    {
+        if(0 < nodeNumber || nodeNumber <= 999)
+        {
+            return false;
+        }
+
+        SendCommand($"$N{nodeNumber}");
+        return true;
+    }
+
+    public void ChangeDate(DateOnly date)
+    {
+        StringBuilder command = new StringBuilder();
+        command.Append("$D");
+
+        SendCommand(command.ToString());
+    }
+
+    public void ChangeTime(TimeOnly time)
+    {
+        StringBuilder command = new StringBuilder();
+        command.Append("$");
+
+        SendCommand(command.ToString());
+    }
+
+    public bool ChangeInterval(int intervalSeconds)
+    {
+        StringBuilder command = new StringBuilder();
+        command.Append("$");
+
+        SendCommand(command.ToString());
+        return true;
+    }
+
+    public bool ChangeDigitalValues(int pin, int value)
+    {
+        StringBuilder command = new StringBuilder();
+        command.Append("$");
+
+        SendCommand(command.ToString());
+        return true;
+    }
+
+    public void ShowMessageOnChanges(bool show)
+    {
+        StringBuilder command = new StringBuilder();
+        command.Append("$");
+
+        SendCommand(command.ToString());
+    }
+
+    private void SendCommand(string command)
+    {
+
+    }
+
 }
