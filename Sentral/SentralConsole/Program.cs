9d7e99e4ba7c3411b9509a356bdd45d2ef677af6
Kristian Klette <geferg.dev@gmail.com>
2023-11-17 15:55:13 +0100

    Create skeleton for db class

--- Sentral/SentralConsole/Program.cs
+++ Sentral/SentralConsole/Program.cs
@@ -335,6 +335,7 @@ internal class Program
             }
         }
 
+        //TODO trim
         return emailInput!;
     }
 
---
+++ Sentral/SentralLibrary/DatabaseConnection.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentralLibrary;
+public class DatabaseConnection
+{
+    private NpgsqlConnection connection;
+
+    public DatabaseConnection(string hostIp, string username, string password, string database)
+    {
+        string connectionString = $"Host={hostIp};Username={username};Password={password};Database={database}";
+        connection = new(connectionString);
+    }
+
+    public UserData? GetUserData(string id)
+    {
+        //TODO implement
+        return new UserData();
+    }
+
+}
