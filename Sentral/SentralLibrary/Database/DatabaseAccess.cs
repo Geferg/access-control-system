802f10d19bde56379230345bf1055cac92a62ad2
Kristian Klette <geferg.dev@gmail.com>
2023-11-21 05:16:49 +0100

    Move database access and connection to new classes

--- Sentral/SentralLibrary/Database/DatabaseAccess.cs
+++ Sentral/SentralLibrary/Database/DatabaseAccess.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SentralLibrary.Database;
-internal class DatabaseAccess
+public class DatabaseAccess
 {
+    private readonly DatabaseConnection databaseConnection;
+
+    public DatabaseAccess(DatabaseConnection databaseConnection)
+    {
+        this.databaseConnection = databaseConnection;
+    }
+
+    public DataTable ExecuteQuery(string query, Dictionary<string, object> parameters)
+    {
+        using var connection = databaseConnection.GetOpenConnection();
+        using var command = new NpgsqlCommand(query, connection);
+        foreach (var parameter in parameters)
+        {
+            command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+        }
+
+        using var adapter = new NpgsqlDataAdapter(command);
+        var dataTable = new DataTable();
+        adapter.Fill(dataTable);
+        return dataTable;
+    }
 }
--- Sentral/SentralLibrary/Database/DatabaseConnection.cs
+++ Sentral/SentralLibrary/Database/DatabaseConnection.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SentralLibrary.Database;
-internal class DatabaseConnection
+public class DatabaseConnection
 {
+    public string HostIp { get; }
+    public string HostPort { get; }
+    public string DatabaseName { get; }
+
+    private readonly string connectionString;
+
+    public DatabaseConnection(string hostIp, string database, string hostPort, string username, string password)
+    {
+        HostIp = hostIp;
+        HostPort = hostPort;
+        DatabaseName = database;
+        connectionString = $"Host={hostIp};Port={hostPort};Username={username};Password={password};Database={database}";
+    }
+
+    public NpgsqlConnection GetOpenConnection()
+    {
+        NpgsqlConnection connection = new(connectionString);
+        connection.Open();
+        return connection;
+    }
+
+    public bool TestConnection()
+    {
+        try
+        {
+            using var connection = GetOpenConnection();
+            // Optionally, execute a simple query like "SELECT 1" to test the connection
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
 }
